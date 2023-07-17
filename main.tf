204bb5c67ea6fa69e5a637d8ba7a5659062b7395
GitHuberkata <petyagavrilova86@gmail.com>
2023-07-17 14:35:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 resource "null_resource" "screen_output" {
 count = 4
   provisioner "local-exec" {
-    command = <<EOT 
+    command = <<-EOT 
     "aws s3 sync s3://reach-sitebuilder/website_v2.6.0.13-development s3://gryphon-tst-reach-engagement-hub/website_v2.6.0.13-development --source-region ap-southeast-2 --region ap-southeast-2 --quiet --debug"
     # echo 'Hello'
     EOT
