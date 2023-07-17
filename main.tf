607447196df0d42944a7d0c4695d0ebd6ac5440b
GitHuberkata <petyagavrilova86@gmail.com>
2023-07-17 14:32:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ resource "null_resource" "screen_output" {
 count = 4
   provisioner "local-exec" {
     command = <<EOT 
-    aws s3 sync s3://reach-sitebuilder/website_v2.6.0.13-development s3://gryphon-tst-reach-engagement-hub/website_v2.6.0.13-development --source-region ap-southeast-2 --region ap-southeast-2 --quiet --debug
-    echo 'Hello'
+    'aws s3 sync s3://reach-sitebuilder/website_v2.6.0.13-development s3://gryphon-tst-reach-engagement-hub/website_v2.6.0.13-development --source-region ap-southeast-2 --region ap-southeast-2 --quiet --debug'
+    # echo 'Hello'
     EOT
   }
 }
